77e50adf732759c9f37ce50c1d3bcb75834285ec
Stefano Rasi <rasi.stefano@gmail.com>
2025-01-13 02:09:35 +0100

    Add SpectrumAnalyzer to song.py

--- song.py
+++ song.py
@@ -4,36 +4,12 @@ import time
 import argparse
 import importlib.util
 
+import scipy
 import numpy as np
+import pygame
 import pyaudio
 
 from threading import Thread
-
-sys.path.insert(0, 'lib')
-
-parser = argparse.ArgumentParser(prog='song')
-
-parser.add_argument('path', nargs='?')
-parser.add_argument('--bpm', type=int, default=120)
-parser.add_argument('--sample_rate', type=int, default=44100)
-
-args = parser.parse_args()
-
-bpm = args.bpm
-path = args.path
-sample_rate = args.sample_rate
-
-basename = os.path.basename(path)
-
-spec = importlib.util.spec_from_file_location(basename, path)
-
-module = importlib.util.module_from_spec(spec)
-
-sys.modules[basename] = module
-
-spec.loader.exec_module(module)
-
-song = module.Song(sample_rate)
 
 t = 0
 
@@ -42,15 +18,13 @@ events = []
 def seconds(s):
     return s * sample_rate
 
-class EventLoopThread(Thread):
+class TrackThread(Thread):
     def __init__(self, track):
         super().__init__()
 
         self.track = track
 
     def run(self):
-        global t, events
-
         t1 = 0
 
         self.running = True
@@ -90,19 +64,105 @@ class EventLoopThread(Thread):
     def stop(self):
         self.running = False
 
-threads = []
+class SpectrumAnalyzer:
+    def __init__(self, width=1024, height=512, min_freq=20, max_freq=20000):
+        pygame.display.init()
+
+        pygame.display.set_caption('Spectrum')
+
+        self.spectrum = None
+
+        self.min_freq = min_freq
+        self.max_freq = max_freq
+
+        self.samples = np.zeros(max_freq)
+
+        self.surface = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+
+    def add_samples(self, samples):
+        self.samples = np.roll(self.samples, -len(samples))
+
+        self.samples[-len(samples):] = samples
+
+        thread = Thread(target=self.make_spectrum)
+
+        thread.start()
+
+    def make_spectrum(self):
+        ifft = scipy.fftpack.rfft(self.samples)
+
+        spectrum = ifft[self.min_freq:]
+
+        xs = np.linspace(0, 1, len(spectrum))
+
+        ys = np.abs(spectrum) / (len(self.samples) / 2)
+
+        freq_range = self.max_freq - self.min_freq
+
+        log_xs = (np.log2(xs * freq_range + self.min_freq) - np.log2(self.min_freq)) / (np.log2(self.max_freq) - np.log2(self.min_freq))
+
+        interpolate = scipy.interpolate.interp1d(log_xs, ys, fill_value='extrapolate')
+
+        width = self.surface.get_width()
+
+        self.spectrum = interpolate(np.linspace(0, 1, width))
+
+    def refresh(self):
+        self.surface.fill(0)
+
+        height = self.surface.get_height()
+
+        if self.spectrum is not None:
+            for x, y in enumerate(self.spectrum):
+                pygame.draw.rect(
+                    self.surface,
+                    (255, 255, 255),
+                    [x, height - (y * height * 4), 1, height]
+                )
+
+            pygame.display.flip()
+
+sys.path.insert(0, 'lib')
+
+parser = argparse.ArgumentParser(prog='song')
+
+parser.add_argument('path', nargs='?')
+parser.add_argument('--bpm', type=int, default=120)
+parser.add_argument('--sample_rate', type=int, default=44100)
+
+args = parser.parse_args()
+
+bpm = args.bpm
+path = args.path
+sample_rate = args.sample_rate
+
+basename = os.path.basename(path)
+
+spec = importlib.util.spec_from_file_location(basename, path)
+
+module = importlib.util.module_from_spec(spec)
+
+sys.modules[basename] = module
+
+spec.loader.exec_module(module)
+
+song = module.Song(sample_rate)
+
+track_threads = []
 
 for track in song.tracks:
-    thread = EventLoopThread(track)
+    track_thread = TrackThread(track)
 
-    threads.append(thread)
+    track_threads.append(track_thread)
 
-    thread.start()
+    track_thread.start()
 
 sounds = []
 
+spectrum_analyzer = SpectrumAnalyzer()
+
 def callback(in_data, frame_count, time_info, status):
-    global t, events, sounds
+    global t
 
     for event in events[:]:
         if event['time'] <= t:
@@ -122,6 +182,8 @@ def callback(in_data, frame_count, time_info, status):
 
     t += frame_count
 
+    spectrum_analyzer.add_samples(samples)
+
     return (samples.clip(-1, 1).astype(np.float32).tobytes(), pyaudio.paContinue)
 
 p = pyaudio.PyAudio()
@@ -136,7 +198,9 @@ stream = p.open(
 
 mtime = os.path.getmtime(path)
 
-while True:
+running = True
+
+while running:
     new_mtime = os.path.getmtime(path)
 
     if new_mtime != mtime:
@@ -146,27 +210,28 @@ while True:
 
         song = module.Song(sample_rate)
 
-        for thread in threads:
-            thread.stop()
+        for track_thread in track_threads:
+            track_thread.stop()
 
         events.clear()
 
-        threads.clear()
+        track_threads.clear()
 
         for track in song.tracks:
-            thread = EventLoopThread(track)
+            track_thread = TrackThread(track)
 
-            threads.append(thread)
+            track_threads.append(track_thread)
 
-            thread.start()
-    else:
-        try:
-            time.sleep(0.1)
-        except:
-            break
+            track_thread.start()
 
-for thread in threads:
-    thread.stop()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    spectrum_analyzer.refresh()
+
+for track_thread in track_threads:
+    track_thread.stop()
 
 stream.close()
 
