829e30705323d7396989be46def59b1359027400
Stefano Rasi <rasi.stefano@gmail.com>
2025-01-15 05:13:50 +0100

    Add convolution effect

---
+++ lib/convolution.py
@@ -0,0 +1,23 @@
+import numpy as np
+import scipy
+
+class Convolution:
+    def __init__(self, impulse):
+        self.window = np.zeros(len(impulse))
+
+        self.impulse_fft = scipy.fft.rfft(impulse)
+
+        self.max_impulse_fft = np.amax(self.impulse_fft)
+
+    def process(self, samples):
+        self.window = np.roll(self.window, -len(samples))
+
+        self.window[-len(samples):] = samples
+
+        window_fft = scipy.fft.rfft(self.window)
+
+        convolution_fft = window_fft * self.impulse_fft / self.max_impulse_fft
+
+        convolution = scipy.fft.irfft(convolution_fft)
+
+        return convolution[-len(samples):]
