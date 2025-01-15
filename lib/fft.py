a448cd33a7acb8b2ce3084f56c0557db382f728f
Stefano Rasi <rasi.stefano@gmail.com>
2025-01-15 05:13:55 +0100

    Clean up

--- lib/fft.py
+++
@@ -1,9 +0,0 @@
-import scipy
-
-def ifft(half_fft):
-    fft = np.concatenate((half_fft, np.flipud(np.conj(half_fft[1:]))))
-
-    return scipy.fftpack.ifft(fft).real * (len(ifft) / 2)
-
-def irfft(fft):
-    return scipy.fftpack.irfft(fft) * (len(fft) / 2)
